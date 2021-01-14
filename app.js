6191f801739476a7d79cfd8381991c7f3fa12dad
robhern135 <robertjhern@gmail.com>
2021-01-14 20:54:03 +0000

    database link

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', ( req,res ) => {
 
 //Connect to DB
 mongoose.connect(
-  process.env.DB_CONNECTION,
+  process.env.MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true  },
   () => console.log('connected to db')
 )
