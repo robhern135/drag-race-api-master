724a40ac187fc851c20e9c3a0a8409b245968aa8
robhern135 <robertjhern@gmail.com>
2021-01-15 09:59:01 +0000

    adding index.html and routing

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path')
+const router = express.Router();
 
 require('dotenv/config')
 
 //Middleware
 app.use(cors())
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 
 //Import Routes
 const queensRoute = require('./routes/queens')
 
+app.use('/', router)
 app.use('/api/queens', queensRoute)
+
+//Index.html
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 //Home Route
 app.get('/api', ( req,res ) => {
